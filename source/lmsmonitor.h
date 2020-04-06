ff712c37c3a69df15bbf8217f294a54efbb40841
Stuart Hunter <stuart.hunter@comcast.net>
2020-04-05 23:29:35 -0400

    expose playing and visualize

---
+++ source/lmsmonitor.h
@@ -0,0 +1,28 @@
+/*
+ *	lmsmonitor.h
+ *
+ *	(c) 2020 Stuart Hunter
+ *
+ *	TODO:	
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	See <http://www.gnu.org/licenses/> to get a copy of the GNU General
+ *	Public License.
+ *
+ */
+
+#ifndef LMSMON_H
+#define LMSMON_H
+
+bool isPlaying(void);
+
+#endif
