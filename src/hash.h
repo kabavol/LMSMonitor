09b9b47e28050a23f6b31bc8d0c55336b75eb2d1
Stuart Hunter <stuart.hunter@comcast.net>
2020-04-18 13:39:47 -0400

    additional viz modes, support routines

---
+++ src/hash.h
@@ -0,0 +1,61 @@
+/*
+ *	metrics.h
+ *
+ *	(c) 2020 Stuart Hunter
+ *
+ *	TODO:	
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	See <http://www.gnu.org/licenses/> to get a copy of the GNU General
+ *	Public License.
+ *
+ */
+
+#ifndef HASHSH_H
+#define HASHSH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ typedef struct {
+    int size;
+    void **keys;
+    void **values;
+} hash_t;
+ 
+hash_t *hash_new (int size) {
+    hash_t *h = calloc(1, sizeof (hash_t));
+    h->keys = calloc(size, sizeof (void *));
+    h->values = calloc(size, sizeof (void *));
+    h->size = size;
+    return h;
+}
+ 
+int hash_index (hash_t *h, void *key) {
+    int i = (int)key % h->size;
+    while (h->keys[i] && h->keys[i] != key)
+        i = (i + 1) % h->size;
+    return i;
+}
+ 
+void hash_insert (hash_t *h, void *key, void *value) {
+    int i = hash_index(h, key);
+    h->keys[i] = key;
+    h->values[i] = value;
+}
+ 
+void *hash_lookup (hash_t *h, void *key) {
+    int i = hash_index(h, key);
+    return h->values[i];
+}
+ 
+#endif
---
+++ src/visdata.c
@@ -0,0 +1,47 @@
+/*
+ *	(c) 2020 Stuart Hunter
+ *
+ *	TODO:
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	See <http://www.gnu.org/licenses/> to get a copy of the GNU General
+ *	Public License.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "visdata.h"
+
+const int getMode(const char *mode)
+{
+    if (0 == strncmp(mode, VMODE_VU, 2))
+        return VEMODE_VU;
+    else if (0 == strncmp(mode, VMODE_PK, 2))
+        return VEMODE_PK;
+    else if (0 == strncmp(mode, VMODE_SA, 2))
+        return VEMODE_SA;
+    else if (0 == strncmp(mode, VMODE_ST, 2))
+        return VEMODE_ST;
+    else if (0 == strncmp(mode, VMODE_SM, 2))
+        return VEMODE_SM;
+    else if (0 == strncmp(mode, VMODE_A1, 2))
+        return VEMODE_A1;
+    else if (0 == strncmp(mode, VMODE_TY, 2))
+        return VEMODE_TY;
+    else if (0 == strncmp(mode, VMODE_RN, 2))
+        return VEMODE_RN;
+    else
+        return -1;
+}
