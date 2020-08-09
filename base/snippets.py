97379e11b023b89eae1384bfe75a1dac1d6b380a
Stuart Hunter <stuart.hunter@comcast.net>
2020-08-08 21:10:34 -0400

    updated assets, additional eggs + video

--- base/snippets.py
+++ base/snippets.py
@@ -20,9 +20,10 @@ def describeArc(x, y, radius, startAngle, endAngle):
 
     d = [
         "M", str(start['x']), str(start['y']),
-        "A", str(radius), str(radius), '0', largeArcFlag, '0', str(end['x']), str(end['y'])
+        "A", str(radius), str(radius), '0', largeArcFlag, '0', str(
+            end['x']), str(end['y'])
     ]
-    
+
     return ' '.join(d)
 
 
@@ -83,7 +84,7 @@ for y in range(0, 12):
     if y > 0:
         z = (2*y) + 23
     print(f'<use xlink:href="#arm" x="{x}" y="{3+(54*yy)}" ',
-        f'transform="rotate({z} {x+4} {14+(54*yy)})" fill="white"/>')
+          f'transform="rotate({z} {x+4} {14+(54*yy)})" fill="white"/>')
     yy += 1
 print('</svg>')
 
@@ -101,13 +102,15 @@ print(f'''<svg width="{siz}" height="{siz*cnt}"
 
 for y in range(0, 5):
     a = 72 * y
-    print(f'<use xlink:href="#ko" x="7.5" y="8.5" transform="rotate({a} {siz/2} {siz/2})"/>')
+    print(
+        f'<use xlink:href="#ko" x="7.5" y="8.5" transform="rotate({a} {siz/2} {siz/2})"/>')
 
 print(f'<circle fill="black" r="4.5" cx="{siz/2}" cy="{siz/2}"/>')
 
 for y in range(0, 6):
     a = 60 * y
-    print(f'<circle fill="white" r="1" cx="{(siz/2)-3}" cy="{siz/2}" transform="rotate({a} {siz/2} {siz/2})"/>')
+    print(
+        f'<circle fill="white" r="1" cx="{(siz/2)-3}" cy="{siz/2}" transform="rotate({a} {siz/2} {siz/2})"/>')
 
 print(f'''
 <line fill="none" stroke-linecap="round" stroke="black" stroke-width="2" x1="0" y1="{siz/2}" x2="6" y2="{siz/2}"/>
@@ -119,6 +122,89 @@ print(f'''
 for y in range(0, cnt):
     a = 20 * y
     z = (siz*(1+y))-(siz/2)
-    print(f'<use xlink:href="#reel" x="0" y="{siz*y}" width="{siz}" transform="rotate({a} {siz/2} {z})"/>')
+    print(
+        f'<use xlink:href="#reel" x="0" y="{siz*y}" width="{siz}" transform="rotate({a} {siz/2} {z})"/>')
 
+print('</svg>')
+
+# vent layout for 90x150 emclosure lid
+h = 90
+w = 150
+print(f'''<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="{w}mm" height="{h}mm" version="1.1"
+     viewBox="0 0 {w} {h}" xmlns="http://www.w3.org/2000/svg">''')
+print(f'<g><rect fill="white" width="{w}" height="{h}" x="0" y="0"/>')
+# 5V fan
+screwspacing = 24
+lpos = (w-screwspacing)/2.00
+rpos = (w+screwspacing)/2.00
+ypos = 70
+r = 3.50/2.00
+
+print(f'''<defs>
+<symbol id="vent">
+<line x1="2" y1="2" x2="2" y2="17" fill="none" stroke-linecap="round" stroke="red" stroke-width="2.8"/>
+<line y1="27" x1="2" y2="42" x2="2" fill="none" stroke-linecap="round" stroke="red" stroke-width="2.8"/>
+<line y1="52" x1="2" y2="67" x2="2" fill="none" stroke-linecap="round" stroke="red" stroke-width="2.8"/>
+</symbol>
+</defs>
+<g id="fanKnockout">
+   <circle fill="black" cy="{lpos}" cx="{ypos}" r="{r}"/>
+   <circle fill="black" cy="{rpos}" cx="{ypos}" r="{r}"/>
+   <circle fill="black" cy="{lpos}" cx="{ypos+screwspacing}" r="{r}"/>
+   <circle fill="black" cy="{rpos}" cx="{ypos+screwspacing}" r="{r}"/>
+''')
+
+cxpos = w/2.00
+cypos = ypos+(screwspacing/2.00)
+c = "black"
+sw = 3
+
+iter = 8
+turn = 360/iter
+for y in range(0, iter):
+    sw = 3
+    r = 13
+    degs = (turn * (iter/r)) / 2.00
+    ha = degs + (turn*y)
+    la = ha - (degs * 2)
+    print(
+        #f'    <path fill="none" stroke-linecap="round" stroke="{c}" stroke-width="{sw}" d="{describeArc(cxpos, cypos, r, la, ha)}"/>')
+        f'    <path fill="none" stroke-linecap="round" stroke="{c}" stroke-width="{sw}" d="{describeArc(cypos, cxpos, r, la, ha)}"/>')
+    r = 9
+    #sw -= .5
+    degs = 9.5 # (turn * (iter/((y+1)*r))) / 2.00
+    ha = degs + (turn*y)
+    la = ha - (degs * 2)
+    print(
+        #f'    <path fill="none" stroke-linecap="round" stroke="{c}" stroke-width="{sw}" d="{describeArc(cxpos, cypos, r, la, ha)}"/>')
+        f'    <path fill="none" stroke-linecap="round" stroke="{c}" stroke-width="{sw}" d="{describeArc(cypos, cxpos, r, la, ha)}"/>')
+    r = 5
+    degs = 2 # (turn * (iter/((y+1)*r))) / 2.00
+    ha = degs + (turn*y)
+    la = ha - (degs * 2)
+    print(
+        #f'    <path fill="none" stroke-linecap="round" stroke="{c}" stroke-width="{sw}" d="{describeArc(cxpos, cypos, r, la, ha)}"/>')
+        f'    <path fill="none" stroke-linecap="round" stroke="{c}" stroke-width="{sw}" d="{describeArc(cypos, cxpos, r, la, ha)}"/>')
+
+print('\n</g>\n<g id="venting">')
+
+l = 65
+mw = w/2.00
+xoff = 4
+xpos = (mw-(l+xoff))
+
+for y in range(0, 5):
+    ypos = 15 + (y*10)
+    print(
+        f'<use xlink:href="#vent" y="{xpos}" x="{ypos}"/>'
+    )
+    if y < 4:
+        print(
+            f'<use xlink:href="#vent" y="{xpos}" x="{ypos+89}"/>'
+        )
+
+print('</g></g>')
 print('</svg>')
